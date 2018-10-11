c5fff4fb58659f96bc9d68dfe971dff25f9f3478
uufish <uufish.io@gmail.com>
2018-10-12 02:09:15 +0700

    add: storage

---
+++ src/storage/onFinalizeStorageObject.ts
@@ -0,0 +1,44 @@
+import { region } from "firebase-functions";
+import { ObjectMetadata } from "firebase-functions/lib/providers/storage";
+import { FILES, IMAGES } from "../constants/collection";
+import { ASIA_NORTHEAST1 } from "../constants/region";
+import { createImageURL } from "../helpers/createImageURL";
+import { createFile } from "../models/file/createFile";
+import { createImage } from "../models/image/createImage";
+import { document } from "../utils/document";
+import { toFileName } from "../utils/toFileName";
+
+const handler = async (object: ObjectMetadata) => {
+  if (typeof object.name !== "string") {
+    throw new Error("object.name not found");
+  }
+
+  const imageURL = await createImageURL(object.name);
+
+  const imageId = toFileName(object.name);
+
+  const image = createImage({
+    id: imageId,
+    imageURL,
+    bucketName: object.bucket,
+    filePath: object.name
+  });
+
+  await document(IMAGES, imageId).set(image);
+
+  const fileId = toFileName(object.name);
+
+  const file = createFile({
+    id: fileId,
+    bucketName: object.bucket,
+    contentType: object.contentType as string,
+    filePath: object.name,
+    size: Number(object.size)
+  });
+
+  await document(FILES, fileId).set(file);
+};
+
+export = region(ASIA_NORTHEAST1)
+  .storage.object()
+  .onFinalize(handler);
