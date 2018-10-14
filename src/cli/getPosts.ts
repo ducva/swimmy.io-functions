de9f5ac0c6d204365e0cce5f49c8155ac1dd5bda
uufish <uufish.io@gmail.com>
2018-10-15 00:42:39 +0700

    add: cli

---
+++ src/cli/getPosts.ts
@@ -0,0 +1,25 @@
+import { credential, firestore, initializeApp } from "firebase-admin";
+import { POSTS } from "../constants/collection";
+import { collection } from "../utils/collection";
+import { getConfig } from "../utils/getConfig";
+
+const main = async () => {
+  const config = getConfig("umfzwkzvrtpe");
+
+  initializeApp(
+    {
+      credential: credential.cert(config.firebase)
+    },
+    "sw"
+  );
+
+  firestore().settings({ timestampsInSnapshots: true });
+
+  const posts = await collection(POSTS).get();
+
+  console.log("length", posts.docs.length);
+};
+
+main().catch(err => {
+  console.error(err);
+});
