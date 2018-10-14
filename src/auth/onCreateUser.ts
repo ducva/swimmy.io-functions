c2c4008aa61c3cd809bcc4f1451bafe78aefef26
uufish <uufish.io@gmail.com>
2018-10-14 14:54:04 +0700

    add: auth

---
+++ src/auth/onCreateUser.ts
@@ -0,0 +1,23 @@
+import { auth } from "firebase-admin";
+import { region } from "firebase-functions";
+import { USERS } from "../constants/collection";
+import { ASIA_NORTHEAST1 } from "../constants/region";
+import { createUser } from "../models/user/createUser";
+import { document } from "../utils/document";
+
+const handler = async (userRecord: auth.UserRecord) => {
+  const isSwUser = !!userRecord.email.includes("@swimmy.io");
+
+  const user = createUser({
+    userId: userRecord.uid,
+    username: isSwUser
+      ? userRecord.email.replace("@swimmy.io", "")
+      : userRecord.uid
+  });
+
+  await document(USERS, userRecord.uid).set(user);
+};
+
+export = region(ASIA_NORTHEAST1)
+  .auth.user()
+  .onCreate(handler);
---
+++ src/auth/onDeleteUser.ts
@@ -0,0 +1,13 @@
+import { auth } from "firebase-admin";
+import { region } from "firebase-functions";
+import { USERS } from "../constants/collection";
+import { ASIA_NORTHEAST1 } from "../constants/region";
+import { document } from "../utils/document";
+
+const handler = async (userRecord: auth.UserRecord) => {
+  await document(USERS, userRecord.uid).delete();
+};
+
+export = region(ASIA_NORTHEAST1)
+  .auth.user()
+  .onDelete(handler);
