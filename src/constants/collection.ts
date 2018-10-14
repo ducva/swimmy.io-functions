982b6521044092f6c421301351a06d33db083e99
uufish <uufish.io@gmail.com>
2018-10-14 14:54:12 +0700

    update

--- src/constants/collection.ts
+++ src/constants/collection.ts
@@ -1,3 +1,7 @@
-export const POSTS = "posts";
-export const USERS = "users";
+export const FILES = "files";
 export const IMAGES = "images";
+export const POSTS = "posts";
+export const POSTS_AS_ANONYM = "posts-as-anonym";
+export const POSTS_AS_THREAD = "posts-as-thread";
+export const POSTS_AS_IMAGE = "posts-as-image";
+export const USERS = "users";
